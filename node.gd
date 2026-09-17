484b635781a31a500fa05f8b0b017b2d151f1a8d
duaneto <duaneto3734@gmail.com>
2026-09-16 21:22:37 -0300

    desing_changes
    darker sprites to jacket and bullet, new start position, camera is now far from jacket and the design of the map was barely changed

--- node.gd
+++ node.gd
@@ -4,11 +4,11 @@ var tamTela: Vector2
 
 
 func _ready() -> void:
-	$Jogador.position.x = 600
-	$Jogador.position.y = 400
+	$Jogador.position.x = 1200
+	$Jogador.position.y = 1200
 
 
 func _process(delta: float) -> void:
-	$VideoStreamPlayer.position.x = $Jogador.position.x - 250
+	$VideoStreamPlayer.position.x = $Jogador.position.x - 310
 	$VideoStreamPlayer.position.y = $Jogador.position.y - 200
 	
